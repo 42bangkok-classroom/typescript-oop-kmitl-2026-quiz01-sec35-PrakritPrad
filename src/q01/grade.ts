c85e0dff34a69e12fd2f07fc0e57cd4d412f7879
Prakrit khwanphatthalung <poom66310@gmail.com>
2025-12-15 09:46:19 +0700

    1

--- src/q01/grade.ts
+++ src/q01/grade.ts
@@ -5,23 +5,23 @@ if(Number.isNaN(Input1) || Input1 < 0){
    process.exit();
 }
 if(Input1 >= 50 && Input1 < 60){
-  console.log("D");
+  console.log("Grade is D");
 }
 
 else if(Input1 >+ 60 && Input1 < 70){
-  console.log("C");
+  console.log("Grade is C");
 }
 
 else if(Input1 >= 70 && Input1 < 80){
-  console.log("B");
+  console.log("Grade is B");
 }
 
 else if(Input1 >= 80 && Input1 < 100){
-  console.log("A");
+  console.log("Grade is A");
 }
 
 else{
-  console.log("F");
+  console.log("Grade is F");
 }
 
 export {};
