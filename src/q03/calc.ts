7691cc79cfdcdad5f0bb29cb875b5527e735e58d
Prakrit khwanphatthalung <poom66310@gmail.com>
2026-01-20 19:29:21 +0700

    test16

--- src/q03/calc.ts
+++ src/q03/calc.ts
@@ -2,7 +2,7 @@ let op : string = process.argv[2].toLowerCase().trim() ?? "";
 let a = parseFloat(process.argv[3]);
 let b = parseFloat(process.argv[4]);
 
-if(Number.isNaN(a) || a < 0 || Number.isNaN(b) || b < 0 || process.argv[2] == " " || process.argv[3] == " " || process.argv[4] == " "){
+if(Number.isNaN(a) || a < 0 || Number.isNaN(b) || b < 0 || process.argv[2] == " " || process.argv[3] == " " || process.argv[4] == " " || (process.argv[2] == " " || process.argv[3] == " " || process.argv[4] == " ")){
    console.log("Invalid input");
    process.exit();
 }
