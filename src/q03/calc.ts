fb34a956affb4e70d6678f6602b87bb8034fb297
Prakrit khwanphatthalung <poom66310@gmail.com>
2026-01-20 19:31:42 +0700

    test16

--- src/q03/calc.ts
+++ src/q03/calc.ts
@@ -1,10 +1,11 @@
-let op : string = process.argv[2].toLowerCase().trim() ?? "";
+let op: string = (process.argv[2] ?? "").toLowerCase().trim();
+
 let a = parseFloat(process.argv[3]);
 let b = parseFloat(process.argv[4]);
 
-if(Number.isNaN(a) || a < 0 || Number.isNaN(b) || b < 0 || process.argv[2] == " " || process.argv[3] == " " || process.argv[4] == " " || (process.argv[2] == " " || process.argv[3] == " " || process.argv[4] == " ")){
-   console.log("Invalid input");
-   process.exit();
+if (Number.isNaN(a) || a < 0 || Number.isNaN(b) || b < 0 || process.argv[2] == " " || process.argv[3] == " " || process.argv[4] == " " || (process.argv[2] == " " || process.argv[3] == " " || process.argv[4] == " ")) {
+  console.log("Invalid input");
+  process.exit();
 }
 
 
@@ -19,7 +20,7 @@ switch (op) {
     console.log(`${a * b}`);
     break
   case "div":
-    if(b === 0){
+    if (b === 0) {
       console.log("Invalid input");
       break
     }
