a4a64f15c4e8e8fad376d77f6811a760adc011ed
Prakrit khwanphatthalung <poom66310@gmail.com>
2026-01-20 18:52:04 +0700

    test08

--- src/q04/sum.ts
+++ src/q04/sum.ts
@@ -1,6 +1,6 @@
 let word = (process.argv[2] ?? "").trim(); 
 const N = Number(word)
-if(Number.isNaN(N) || N <= 0 || process.argv[2] == ' ' || !Number.isInteger(N)){
+if(Number.isNaN(N) || N <= 0 || N == 0 || process.argv[2] == ' ' || !Number.isInteger(N)){
    console.log("Invalid Input");
    process.exit();
 }
