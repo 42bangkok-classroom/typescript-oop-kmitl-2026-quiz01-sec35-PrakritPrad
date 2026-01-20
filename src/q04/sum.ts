ef48bb756dd173ccd67d9b925bdf0e3d921137c1
Prakrit khwanphatthalung <poom66310@gmail.com>
2026-01-20 18:31:01 +0700

    test03

--- src/q04/sum.ts
+++ src/q04/sum.ts
@@ -1,6 +1,6 @@
 let Num = Number(process.argv[2]);  
 
-if(Number.isNaN(Num) || Num < 0){
+if(Number.isNaN(Num) || Num < 0 || process.argv[2] == " " || !Number.isInteger(Num)){
    console.log("Invalid Input");
    process.exit();
 }
@@ -9,6 +9,6 @@ let sum = 0;
 for(let i = Num;i >= 0;i--){
   sum += i;
 }
-console.log(sum);
+console.log("Sum: ",sum);
 
 export{};
