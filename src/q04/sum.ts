064d5a643416233a2faa04bb23e08bd1fcddf31e
Prakrit khwanphatthalung <poom66310@gmail.com>
2026-01-20 18:42:46 +0700

    test06

--- src/q04/sum.ts
+++ src/q04/sum.ts
@@ -1,14 +1,15 @@
-let Num = Number(process.argv[2]);  
-
-if(Number.isNaN(Num) || Num < 0 || process.argv[2] == " " || !Number.isInteger(Num)){
+let word = (process.argv[2] ?? "").trim(); 
+const N = Number(word)
+if(Number.isNaN(N) || N < 0 || process.argv[2] == " " || !Number.isInteger(N)){
    console.log("Invalid Input");
    process.exit();
 }
 
 let sum = 0;
-for(let i = Num;i >= 0;i--){
+for(let i = N;i >= 0;i--){
   sum += i;
 }
-console.log("Sum:",sum);
+console.log(`Sum: ${sum}`);
+
 
 export{};
